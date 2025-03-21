a2112c77773cd3611765286a67a0aff816fd2c84
Tyler055 <darkneuralai@gmail.com>
2025-03-20 22:50:29 -0500

    ok

--- backend/src/config/env.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-const config = {
-  PORT: process.env.PORT || 5000,
-  MONGO_URI: process.env.MONGO_URI,
-  JWT_SECRET: process.env.JWT_SECRET || "defaultsecret",
-};
-
-module.exports = config;
