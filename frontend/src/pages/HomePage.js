30b1dde29d9e964e2cd81d39018dfe0efebafec4
Tyler055 <darkneuralai@gmail.com>
2025-03-21 00:16:28 -0500

    0

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import HabitForm from "../components/HabitForm";
+import HabitList from "../components/HabitList";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Habit Tracker</h1>
+      <HabitForm onHabitAdded={() => window.location.reload()} />
+      <HabitList />
+    </div>
+  );
+}
